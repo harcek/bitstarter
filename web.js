047a549e5886b69829948acb1d382d4712cfeaed
Daniel Harcek <daniel@harcek.info>
2013-07-11 22:36:23 +0000

    adding index.html, web.js response reading from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
+var fdata = fs.readFileSync("index.html"); // buffer
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(fdata.toString());
 });
 
 var port = process.env.PORT || 5000;
